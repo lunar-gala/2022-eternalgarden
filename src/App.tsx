d2b093de0029ea36fa3d5064fee3a14b81ef5e52
Bennett Huffman <bhuffman@andrew.cmu.edu>
2022-02-23 14:45:20 -0500

    initial preloader animation + window size hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
-import "./App.css";
-import { Route, Routes } from "react-router-dom";
-import Home from "./routes/home/home";
-import About from "./routes/about/about";
-import NavBar from "./components/navbar/navbar";
-import Preloader from "./components/preloader/preloader"
-import "./styles/main.scss";
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './routes/home/home';
+import About from './routes/about/about';
+import './styles/main.scss';
 
 function App() {
   return (
     <>
       <div className="App">
-        {/* <Preloader /> */}
-        {/* <NavBar /> */}
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
