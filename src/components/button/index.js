d2f9af9c9702b2b96ecda101e76ad6ef92c8e5cf
Kazi Jawad <kazi.a.jawad@gmail.com>
2022-02-25 16:55:23 -0500

    menu layout

---
+++ src/components/button/index.js
@@ -0,0 +1,11 @@
+import { ReactNode } from 'react'
+import cn from 'classnames';
+import styles from './index.module.scss';
+
+export default function Button({ className, onClick, children }) {
+  return (
+    <div className={cn(styles.btn, className)} onClick={onClick}>
+      {children}
+    </div>
+  );
+}
