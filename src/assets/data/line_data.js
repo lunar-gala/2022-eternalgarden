e91425f201bb1a8687262efb37907eaf6c12f3a7
Michelle <mliuu3259@gmail.com>
2022-02-25 20:32:27 -0500

    set up initial line_data.js config file

---
+++ src/assets/data/line_data.js
@@ -0,0 +1,97 @@
+// images for testing... will figure out better way to import images later
+import IMG_1 from '../images/lines/PNTX1062-1.jpeg';
+import IMG_2 from '../images/lines/PNTX1189-1.jpeg';
+
+export const LINE_INFO = [
+  {
+    name: 'expeiri',
+    designers: ['Skylar McAuliffe', 'Asad Sheikh'],
+    images: [IMG_1, IMG_2],
+    description:
+      'Born in 1996, Lunar Gala is an annual student-run fashion show that showcases brilliant artistry in design, modeling, and production, and dance performance. The show was originally inspired by the Taiwanese Student Association’s Lunar New Year tradition of wearing new clothes. Over the years, it has evolved to become the largest fashion event in Pittsburgh, a unique platform to display innovative and avant garde fashion. ',
+  },
+  {
+    name: 'Morphosis',
+    designers: ['Jamie McArthur', 'Mei Leng', 'Vivian Sun'],
+    images: [IMG_1, IMG_2],
+    description:
+      'Born in 1996, Lunar Gala is an annual student-run fashion show that showcases brilliant artistry in design, modeling, and production, and dance performance. The show was originally inspired by the Taiwanese Student Association’s Lunar New Year tradition of wearing new clothes. Over the years, it has evolved to become the largest fashion event in Pittsburgh, a unique platform to display innovative and avant garde fashion. ',
+  },
+  {
+    name: 'Monthly',
+    designers: ['Suzie Liu'],
+    images: [IMG_1, IMG_2],
+    description:
+      'Born in 1996, Lunar Gala is an annual student-run fashion show that showcases brilliant artistry in design, modeling, and production, and dance performance. The show was originally inspired by the Taiwanese Student Association’s Lunar New Year tradition of wearing new clothes. Over the years, it has evolved to become the largest fashion event in Pittsburgh, a unique platform to display innovative and avant garde fashion. ',
+  },
+  {
+    name: 'Earth to bex',
+    designers: ['Rebecca Kim'],
+    images: [IMG_1, IMG_2],
+    description:
+      'Born in 1996, Lunar Gala is an annual student-run fashion show that showcases brilliant artistry in design, modeling, and production, and dance performance. The show was originally inspired by the Taiwanese Student Association’s Lunar New Year tradition of wearing new clothes. Over the years, it has evolved to become the largest fashion event in Pittsburgh, a unique platform to display innovative and avant garde fashion. ',
+  },
+  {
+    name: 'Lian',
+    designers: ['Sunniva Liu', 'Minsung Kang'],
+    images: [IMG_1, IMG_2],
+    description:
+      'Born in 1996, Lunar Gala is an annual student-run fashion show that showcases brilliant artistry in design, modeling, and production, and dance performance. The show was originally inspired by the Taiwanese Student Association’s Lunar New Year tradition of wearing new clothes. Over the years, it has evolved to become the largest fashion event in Pittsburgh, a unique platform to display innovative and avant garde fashion. ',
+  },
+  {
+    name: 'SeaWitch',
+    designers: ['Julie Scharf'],
+    images: [IMG_1, IMG_2],
+    description:
+      'Born in 1996, Lunar Gala is an annual student-run fashion show that showcases brilliant artistry in design, modeling, and production, and dance performance. The show was originally inspired by the Taiwanese Student Association’s Lunar New Year tradition of wearing new clothes. Over the years, it has evolved to become the largest fashion event in Pittsburgh, a unique platform to display innovative and avant garde fashion. ',
+  },
+  {
+    name: 'Levensduur',
+    designers: ['Max White', 'Oscar Monarrez'],
+    images: [IMG_1, IMG_2],
+    description:
+      'Born in 1996, Lunar Gala is an annual student-run fashion show that showcases brilliant artistry in design, modeling, and production, and dance performance. The show was originally inspired by the Taiwanese Student Association’s Lunar New Year tradition of wearing new clothes. Over the years, it has evolved to become the largest fashion event in Pittsburgh, a unique platform to display innovative and avant garde fashion. ',
+  },
+  {
+    name: 'Rites of Passage',
+    designers: ['Emily DuBois', 'Jeanie Xu'],
+    images: [IMG_1, IMG_2],
+    description:
+      'Born in 1996, Lunar Gala is an annual student-run fashion show that showcases brilliant artistry in design, modeling, and production, and dance performance. The show was originally inspired by the Taiwanese Student Association’s Lunar New Year tradition of wearing new clothes. Over the years, it has evolved to become the largest fashion event in Pittsburgh, a unique platform to display innovative and avant garde fashion. ',
+  },
+  {
+    name: 'EVANESCENS',
+    designers: ['Sean Meng'],
+    images: [IMG_1, IMG_2],
+    description:
+      'Born in 1996, Lunar Gala is an annual student-run fashion show that showcases brilliant artistry in design, modeling, and production, and dance performance. The show was originally inspired by the Taiwanese Student Association’s Lunar New Year tradition of wearing new clothes. Over the years, it has evolved to become the largest fashion event in Pittsburgh, a unique platform to display innovative and avant garde fashion. ',
+  },
+  {
+    name: 'World Scanner',
+    designers: ['Isabel Xu', 'Reifen Jin'],
+    images: [IMG_1, IMG_2],
+    description:
+      'Born in 1996, Lunar Gala is an annual student-run fashion show that showcases brilliant artistry in design, modeling, and production, and dance performance. The show was originally inspired by the Taiwanese Student Association’s Lunar New Year tradition of wearing new clothes. Over the years, it has evolved to become the largest fashion event in Pittsburgh, a unique platform to display innovative and avant garde fashion. ',
+  },
+  {
+    name: 'Reverie',
+    designers: ['Somya Pathak'],
+    images: [IMG_1, IMG_2],
+    description:
+      'Born in 1996, Lunar Gala is an annual student-run fashion show that showcases brilliant artistry in design, modeling, and production, and dance performance. The show was originally inspired by the Taiwanese Student Association’s Lunar New Year tradition of wearing new clothes. Over the years, it has evolved to become the largest fashion event in Pittsburgh, a unique platform to display innovative and avant garde fashion. ',
+  },
+  {
+    name: 'Monachopsis',
+    designers: ['Gloria Huang', 'Chloe Wang', 'Mai Tian'],
+    images: [IMG_1, IMG_2],
+    description:
+      'Born in 1996, Lunar Gala is an annual student-run fashion show that showcases brilliant artistry in design, modeling, and production, and dance performance. The show was originally inspired by the Taiwanese Student Association’s Lunar New Year tradition of wearing new clothes. Over the years, it has evolved to become the largest fashion event in Pittsburgh, a unique platform to display innovative and avant garde fashion. ',
+  },
+  {
+    name: 'Alumni Line',
+    designers: ['Collab'],
+    images: [IMG_1, IMG_2],
+    description:
+      'Born in 1996, Lunar Gala is an annual student-run fashion show that showcases brilliant artistry in design, modeling, and production, and dance performance. The show was originally inspired by the Taiwanese Student Association’s Lunar New Year tradition of wearing new clothes. Over the years, it has evolved to become the largest fashion event in Pittsburgh, a unique platform to display innovative and avant garde fashion. ',
+  },
+];
---
+++ src/routes/lines/lines.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {
+  motion,
+  useAnimation,
+  AnimateSharedLayout,
+  AnimatePresence,
+} from 'framer-motion';
+
+import './home.scss';
+import { LINE_INFO } from '../../assets/data/line_data';
+
+export default function Lines() {}
