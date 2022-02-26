fe9f3c3427f03c3e1620d2ec168356ac7699262f
Kazi Jawad <kazi.a.jawad@gmail.com>
2022-02-26 18:55:26 -0500

    base transitions for about page

--- src/routes/about/index.js
+++ src/routes/about/index.js
@@ -1,42 +1,131 @@
 import cn from 'classnames';
 import { Link, useLocation } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 import styles from 'routes/about/index.module.scss';
 import Button from 'components/button';
+
+const transition = {
+  duration: 1,
+  ease: 'easeOut',
+};
+
+const button = {
+  hidden: {
+    scale: 0,
+    opacity: 0,
+  },
+  visible: {
+    scale: 1,
+    opacity: 1,
+  },
+};
+
+const container = {
+  hidden: {
+    opacity: 0,
+  },
+  visible: {
+    opacity: 1
+  },
+}
+
+const items = {
+  hidden: {
+    opacity: 0,
+    transition: {
+      when: 'afterChildren',
+      staggerChildren: 0.3,
+      ease: 'easeOut',
+      delay: 0.3,
+    },
+  },
+  visible: {
+    opacity: 1,
+    transition: {
+      when: 'beforeChildren',
+      staggerChildren: 0.3,
+      delay: 0.8,
+      ease: 'easeOut',
+    },
+  },
+};
+
+const item = {
+  hidden: {
+    opacity: 0
+  },
+  visible: {
+    opacity: 1
+  },
+}
 
 export default function About() {
   const location = useLocation();
 
   return (
     <div>
-      <section className={styles.container}>
+      <motion.section
+        className={styles.container}
+        variants={container}
+        initial="hidden"
+        animate="visible"
+        exit="hidden"
+        transition={{ ...transition, delay: 0 }}
+      >
         <div className={styles.navbar}>
           <Link to="/menu" state={{ previousLocation: '/about' }}>
-            <Button type="white">
-              Explore
-            </Button>
+            <motion.div
+              className={styles.wrapper}
+              variants={button}
+              initial="hidden"
+              animate="visible"
+              exit="hidden"
+              transition={{ ...transition, delay: 0.1 }}
+            >
+              <Button type="white">
+                Explore
+              </Button>
+            </motion.div>
           </Link>
           <Link to="/" state={{ previousLocation: '/about' }}>
-            <Button type="white">
-              Tickets
-            </Button>
+            <motion.div
+              className={styles.wrapper}
+              variants={button}
+              initial="hidden"
+              animate="visible"
+              exit="hidden"
+              transition={{ ...transition, delay: 0.4 }}
+            >
+              <Button type="white">
+                Tickets
+              </Button>
+            </motion.div>
           </Link>
         </div>
-        <h1 className={cn('h2', styles.header)}>
-          ABOUT
-        </h1>
-        <h2 className={cn('h4', styles.subheader)}>
-          EXISTENCE IS INTERDEPENDENT.
-        </h2>
-        <div className={styles.content}>
-          <p className="body">
-            Born in 1996, Lunar Gala is an annual student-run fashion show that showcases brilliant artistry in design, modeling, and production, and dance performance. The show was originally inspired by the Taiwanese Student Association’s Lunar New Year tradition of wearing new clothes. Over the years, it has evolved to become the largest fashion event in Pittsburgh, a unique platform to display innovative and avant.
-          </p>
-          <p className="body">
-            Our designers are unafraid of freely expressing themselves and taking risks. They explore our society’s most pressing current events and cultural values to bring them to life. Some of the unconventional materials that they use range from technological mood-sensing chips or glow-in-the-dark resin to robust sheet metal.
-          </p>
-        </div>
-      </section>
+        <motion.div
+          className={styles.items}
+          variants={items}
+          initial="hidden"
+          animate="visible"
+          exit="hidden"
+        >
+          <motion.h1 className={cn('h2', styles.header)} variants={item}>
+            ABOUT
+          </motion.h1>
+          <motion.h2 className={cn('h4', styles.subheader)} variants={item}>
+            EXISTENCE IS INTERDEPENDENT.
+          </motion.h2>
+          <div className={styles.content}>
+            <motion.p className="body" variants={item}>
+              Born in 1996, Lunar Gala is an annual student-run fashion show that showcases brilliant artistry in design, modeling, and production, and dance performance. The show was originally inspired by the Taiwanese Student Association’s Lunar New Year tradition of wearing new clothes. Over the years, it has evolved to become the largest fashion event in Pittsburgh, a unique platform to display innovative and avant.
+            </motion.p>
+            <motion.p className="body" variants={item}>
+              Our designers are unafraid of freely expressing themselves and taking risks. They explore our society’s most pressing current events and cultural values to bring them to life. Some of the unconventional materials that they use range from technological mood-sensing chips or glow-in-the-dark resin to robust sheet metal.
+            </motion.p>
+          </div>
+        </motion.div>
+      </motion.section>
       <div className={cn('noise', styles.noise)} />
     </div>
   );
