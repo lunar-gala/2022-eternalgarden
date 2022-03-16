b607cefa186f6d7a7e013dd415c2ceed9557fe24
Michelle <mliuu3259@gmail.com>
2022-03-15 22:25:02 -0400

    mobile styling for flowers, cleaned up lottie code

--- src/routes/lines/index.js
+++ src/routes/lines/index.js
@@ -1,107 +1,34 @@
 import React from 'react';
 import { motion, useAnimation } from 'framer-motion';
-import { Link } from 'react-router-dom';
+import Lottie from 'react-lottie';
 
 import './index.scss';
-import Button from '../../components/button/index';
 import Card from '../../components/card/card';
 import { LINE_INFO } from 'assets/lines/line_data';
 import NavBar from 'components/navbar';
-
+import useWindowSize from 'hooks/useWindowSize';
 import BottomGradient from 'components/bottomGradient';
 import {
   transition,
-  button,
   container,
-  leftFlowers,
-  rightFlowers,
+  getFlowerVariants,
+  flowerStagger,
 } from './animation';
-
-//lottie files
-import Lottie from 'react-lottie';
-import purple3flower from 'assets/lines_animation/purple3flower.json';
-import maxBud from 'assets/lines_animation/maxbud.json';
-import sunflower from 'assets/lines_animation/sunflower.json';
-import dragonfly from 'assets/lines_animation/dragonfly.json';
-import pointyflower from 'assets/lines_animation/pointyflower.json';
-
-const flower = {
-  hidden: {
-    opacity: 0,
-  },
-  visible: {
-    opacity: 1,
-  },
-};
-
-const purple3flowerSettings = {
-  loop: true,
-  autoplay: true,
-  animationData: purple3flower,
-  rendererSettings: {
-    preserveAspectRatio: 'xMidYMid slice',
-  },
-};
-
-const dragonflySettings = {
-  loop: true,
-  autoplay: true,
-  animationData: dragonfly,
-  rendererSettings: {
-    preserveAspectRatio: 'xMidYMid slice',
-  },
-};
-
-const maxBudSettings = {
-  loop: true,
-  autoplay: true,
-  animationData: maxBud,
-  rendererSettings: {
-    preserveAspectRatio: 'xMidYMid slice',
-  },
-};
-
-const sunflowerSettings = {
-  loop: true,
-  autoplay: true,
-  animationData: sunflower,
-  rendererSettings: {
-    preserveAspectRatio: 'xMidYMid slice',
-  },
-};
-
-const pointyflowerSettings = {
-  loop: true,
-  autoplay: true,
-  animationData: pointyflower,
-  rendererSettings: {
-    preserveAspectRatio: 'xMidYMid slice',
-  },
-};
-
-const flowerStagger = {
-  hidden: {
-    opacity: 0,
-    transition: {
-      when: 'afterChildren',
-      staggerChildren: 0.05,
-      ease: 'easeOut',
-      delay: 0.07,
-    },
-  },
-  visible: {
-    opacity: 1,
-    transition: {
-      when: 'beforeChildren',
-      staggerChildren: 0.05,
-      delay: 0.07,
-      ease: 'easeOut',
-    },
-  },
-};
+import {
+  flower,
+  purple3flowerSettings,
+  dragonflySettings,
+  maxBudSettings,
+  sunflowerSettings,
+  pointyflowerSettings,
+} from './lottie_settings';
 
 export default function Lines() {
   const controls = useAnimation();
+  const size = useWindowSize();
+  const isMobile = size?.width < 768;
+
+  const { leftFlowers, rightFlowers } = getFlowerVariants(isMobile ? 25 : 65);
 
   return (
     <motion.section
