e3c10d79347b0bc050dfdb89abfc59d1c20ba0ba
Michelle <mliuu3259@gmail.com>
2022-03-15 22:50:05 -0400

    lazy loading?

--- src/components/peopleCard/index.js
+++ src/components/peopleCard/index.js
@@ -3,7 +3,7 @@ import styles from './index.module.scss';
 export default function PeopleCard({ slug, name }) {
   return (
     <div className={styles.card} key={slug}>
-      <img src={`assets/people/${slug}`} alt={name + ' photo'} />
+      <img src={`assets/people/${slug}`} loading="lazy" alt={name + ' photo'} />
       <p>{name}</p>
     </div>
   );
